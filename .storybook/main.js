52606f36bf553b3840f3de128990ffb0f2eff0a2
Nico Neiman <niconeiman@gmail.com>
2022-06-29 14:47:58 -0500

    chore: code cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,5 @@
 module.exports = {
-  stories: [
-    '../**/*.stories.mdx',
-    '../**/*.stories.@(js|jsx|ts|tsx)',
-    '../**/**/*.stories.@(js|jsx|ts|tsx)',
-    '../**/**/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
